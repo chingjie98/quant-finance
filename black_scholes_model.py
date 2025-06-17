04325672699d133b80bee0ecfac46f7803eaa9ac
Ng Ching Jie <35895182+chingjie98@users.noreply.github.com>
2025-06-17 23:38:03 +0800

    implemented main logic of BSM model

--- black_scholes_model.py
+++ black_scholes_model.py
@@ -1,14 +1,48 @@
 
 import numpy as np
-import math
+from math import *
+from scipy import stats
 import time
 
 class OptionPricing:
-    
-	def __init__(self,S0,E,T,rf,sigma,iterations):
-		self.S0 = S0
-		self.E = E
-		self.T = T
-		self.rf = rf
-		self.sigma = sigma     
-		self.iterations = iterations 
+  
+  """
+  S0: Spot price
+  E: Exercise price
+  T = Time to maturity (in years) 
+  rf = risk-free rate
+  sigma = volatility
+  """
+
+  def __init__(self,S0,E,T,rf,sigma):
+    self.S0 = S0            
+    self.E = E              
+    self.T = T             
+    self.rf = rf           
+    self.sigma = sigma    
+  
+  def call_price(self):
+    # d1 accounts for time value and volatility of the underlying asset
+    d1 = (log(self.S0 / self.E) + (self.rf + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * sqrt(self.T))
+    # d2 adjusts d1 downward by the volatility term
+    d2 = d1 - self.sigma * sqrt(self.T)
+    print(f"[Call] d1: {d1}, d2: {d2}")
+    # Black-Scholes call option formula:
+    # Call = S0 * N(d1) - E * e^(-rT) * N(d2)
+    return self.S0 * stats.norm.cdf(d1) - self.E * exp(-self.rf * self.T) * stats.norm.cdf(d2)
+
+  def put_price(self):
+    # d1 accounts for time value and volatility of the underlying asset
+    d1 = (log(self.S0 / self.E) + (self.rf + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * sqrt(self.T))
+    # d2 adjusts d1 downward by the volatility term
+    d2 = d1 - self.sigma * sqrt(self.T)
+    print(f"[Put] d1: {d1}, d2: {d2}")
+    # Black-Scholes put option formula:
+    # Put = -S0 * N(-d1) + E * e^(-rT) * N(-d2)
+    return -self.S0 * stats.norm.cdf(-d1) + self.E * exp(-self.rf * self.T) * stats.norm.cdf(-d2)
+
+
+if __name__ == '__main__':
+  option = OptionPricing(S0=100, E=100, T=1, rf=0.05, sigma=0.2)
+  print("Call Option Price:", option.call_price())
+  print("Put Option Price:", option.put_price())
